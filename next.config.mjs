2dc459abacce13552a03f4451e1131878186f03a
rezaabaskhanian <rezaabaskhanian1367kash@gmail.com>
2026-06-01 11:53:48 +0330

    add assest perfix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  output: 'standalone',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_URL || '',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
